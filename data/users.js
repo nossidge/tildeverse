a5391f067d3f85dee1cfad13173c552aa825bb94
Paul Thompson <nossidge@gmail.com>
2017-08-05 06:03:33 +0100

    add modified date to user table

--- data/users.js
+++ data/users.js
@@ -2,24 +2,26 @@
 // Create the users table in the users tab.
 function users(tildeverseJSON) {
 
-  let thead = "<thead><tr><th>Tilde Box</th><th>User Name</th><th>User URL</th></tr></thead>";
+  let thead = "<thead><tr><th>Tilde Box</th><th>User Name</th><th>User URL</th><th>Modified</th></tr></thead>";
   let tbody = document.createElement("tbody");
   $("#table_users").append(thead);
   $("#table_users").append(tbody);
 
-  let row = '<tr><td><a href="SITE_URL">SITE_NAME</a></td><td>USER_NAME</td><td><a href="USER_URL">USER_URL_TIDY</a></td></tr>';
+  let row = '<tr><td><a href="SITE_URL">SITE_NAME</a></td><td>USER_NAME</td><td><a href="USER_URL">USER_URL_TIDY</a></td><td>MODIFIED</td></tr>';
 
   for (let tildeSite in tildeverseJSON["sites"]) {
     let site = tildeverseJSON["sites"][tildeSite];
     $.each(site["users"], function(user, hash) {
       let url = site["url_format_user"].replace("USER", user);
       let tidy = url.substring(url.indexOf("//") + 2);
+      let time = hash["time"].substring(0, 10);
       let out = row;
       out = out.replace("SITE_URL",      site["url_root"]);
       out = out.replace("SITE_NAME",     tildeSite);
       out = out.replace("USER_NAME",     user);
       out = out.replace("USER_URL",      url);
       out = out.replace("USER_URL_TIDY", tidy.replace(/\/$/, ""));
+      out = out.replace("MODIFIED",      time);
       $(tbody).append(out);
     });
   }
