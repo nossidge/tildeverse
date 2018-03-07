983932d4868e239259baaca261dbe8b94009b953
Paul Thompson <nossidge@gmail.com>
2018-03-07 03:38:11 +0000

    remove check_for_tilde_json

--- lib/tildeverse.rb
+++ lib/tildeverse.rb
@@ -45,7 +45,6 @@ FILES_TO_COPY           = ['users.js', 'boxes.js', 'pie.js']
 
 WRITE_TO_FILES          = true   # This is necessary.
 CHECK_FOR_NEW_BOXES     = false  # This is fast.
-CHECK_FOR_NEW_DESC_JSON = false  # This is slow.
 
 ################################################################################
 
@@ -66,42 +65,9 @@ end
 
 ################################################################################
 
-# Check each Tildebox to see if they have a '/tilde.json' file.
-# I already know that 3 do, so let me know if that number changes.
-#   https://club6.nl/tilde.json
-#   http://ctrl-c.club/tilde.json
-#   https://squiggle.city/tilde.json
-def self.check_for_tilde_json
-
-  # Read from the master list of Tilde URLs, and append '/tilde.json' to them.
-  obj_json = JSON[open(OUTPUT_JSON_TILDEVERSE).read]
-  urls_json = obj_json.keys.map { |i| i + '/tilde.json' }
-
-  # Only select the URLs that can be parsed as JSON.
-  urls_json.select do |item|
-    begin
-      JSON[open(item).read]
-      true
-    rescue StandardError
-      false
-    end
-  end
-end
-
-def self.check_for_new_desc_json
-  tilde_desc_files = check_for_tilde_json
-  return if tilde_desc_files.length == 3
-  puts '-- Tilde Description JSON files:'
-  puts tilde_desc_files
-  puts nil
-end
-
-################################################################################
-
 def self.run_all
   output_to_files if WRITE_TO_FILES
   check_for_new_boxes if CHECK_FOR_NEW_BOXES
-  check_for_new_desc_json if CHECK_FOR_NEW_DESC_JSON
 end
 
 ################################################################################
