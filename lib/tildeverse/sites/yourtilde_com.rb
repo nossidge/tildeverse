84e6bf6da45a276068ae5d08e32dbbf10f154c25
Paul Thompson <nossidge@gmail.com>
2018-04-24 00:58:33 +0100

    update users for 'yourtilde.com'

--- lib/tildeverse/sites/yourtilde_com.rb
+++ lib/tildeverse/sites/yourtilde_com.rb
@@ -19,8 +19,29 @@ module Tildeverse
       def scrape_users
         # There's a strange issue with curling this URL.
         # I'll just use a manual list for now.
-        %w[WL01 deepend emv jovan kingofobsolete login mhj msmcmickey
-           mushmouth nozy sebboh ubergeek]
+        @users = %w[
+          Hustler WL01 caleb copart deepend diverger emv hyperboredoubt
+          jovan khuxkm kingofobsolete login mhj msmcmickey mushmouth
+          nozy oak_tree sebboh ubergeek zenoil
+        ]
+      end
+
+      private
+
+      ##
+      # Here's the scraper code, in case the curl issue is ever fixed:
+      #
+      def actual_scraper_code
+        return @users if @users
+        return @users = [] if con.error?
+
+        # These are lines on the page that start with '<a href'.
+        @users = con.result.split("\n").map do |i|
+          next unless i.strip =~ /^<a href/
+          i.split('~').last.split('<').first
+        end.compact.sort.uniq
+        puts no_user_message if @users.empty?
+        @users
       end
     end
   end
