72d186c3daaccfcf32bb69e78dfb48fa45b4063a
Paul Thompson <nossidge@gmail.com>
2018-05-14 15:06:31 +0100

    add 'get_daily' and 'generate_html' to Config

--- lib/tildeverse/config.rb
+++ lib/tildeverse/config.rb
@@ -15,6 +15,18 @@ module Tildeverse
     attr_reader :get_type
 
     ##
+    # @return [true, false] should new data be automatically downloaded?
+    #
+    attr_reader :get_daily
+    alias get_daily? get_daily
+
+    ##
+    # @return [true, false] should a website be generated?
+    #
+    attr_reader :generate_html
+    alias generate_html? generate_html
+
+    ##
     # @return [Date] date the data was last updated
     #
     attr_reader :updated_on
@@ -26,32 +38,52 @@ module Tildeverse
     def initialize
       if filepath.exist?
         data = YAML.safe_load(filepath.read, [Date])
-        self.get_type = data['get_type']
-        @updated_on   = data['updated_on']
+        @get_type      = validate_get_type      data['get_type']
+        @get_daily     = validate_get_daily     data['get_daily']
+        @generate_html = validate_generate_html data['generate_html']
+        @updated_on    = data['updated_on']
       else
-        @get_type   = 'fetch'
-        @updated_on = Date.new(1970, 1, 1)
+        @get_type      = 'fetch'
+        @get_daily     = true
+        @generate_html = false
+        @updated_on    = Date.new(1970, 1, 1)
       end
       save
     end
 
     ##
-    # @param [String] input
+    # @param [String] value
+    # @return [String] the input value
+    # @raise [ArgumentError] if not valid
     #
-    def get_type=(input)
-      unless types_of_get.include?(input)
-        raise ArgumentError, "Value must be one of: #{types_of_get.join(', ')}"
-      end
-      @get_type = input
-      save
+    def get_type=(value)
+      with(:save) { @get_type = validate_get_type(value) }
     end
 
     ##
-    # Set {#updated_on} to today's date.
+    # @param [true, false] value
+    # @return [true, false] the input value
+    # @raise [ArgumentError] if not valid
+    #
+    def get_daily=(value)
+      with(:save) { @get_daily = validate_get_daily(value) }
+    end
+
+    ##
+    # @param [true, false] value
+    # @return [true, false] the input value
+    # @raise [ArgumentError] if not valid
+    #
+    def generate_html=(value)
+      with(:save) { @generate_html = validate_generate_html(value) }
+    end
+
+    ##
+    # Set {#updated_on} to today's date
+    # @return [Date] today's date
     #
     def update
-      @updated_on = Date.today
-      save
+      with(:save) { @updated_on = Date.today }
     end
 
     ##
@@ -59,13 +91,60 @@ module Tildeverse
     #
     def save
       str = yaml_template
-      %w[get_type updated_on].each do |var|
+      %w[get_type get_daily generate_html updated_on].each do |var|
         str.sub!("@#{var}@", "#{var}:\n  #{send(var)}")
       end
       Files.save_text(str, filepath)
     end
 
     private
+
+    ##
+    # Yield to a block, run a method, and return
+    # the return value of the block.
+    #
+    def with(method_name)
+      output = yield
+      method(method_name).call
+      output
+    end
+
+    ##
+    # Validate that a value is within an array
+    # @return [value] if valid
+    # @raise [ArgumentError] if not valid
+    #
+    def validate_in_array(value, array)
+      return value if array.include?(value)
+      raise ArgumentError, "Value must be one of: #{array.join(', ')}"
+    end
+
+    ##
+    # Validate {#get_type}
+    # @return [String] if valid
+    # @raise [ArgumentError] if not valid
+    #
+    def validate_get_type(value)
+      validate_in_array(value, types_of_get)
+    end
+
+    ##
+    # Validate {#get_daily}
+    # @return [true, false] if valid
+    # @raise [ArgumentError] if not valid
+    #
+    def validate_get_daily(value)
+      validate_in_array(value, [true, false])
+    end
+
+    ##
+    # Validate {#generate_html}
+    # @return [true, false] if valid
+    # @raise [ArgumentError] if not valid
+    #
+    def validate_generate_html(value)
+      validate_in_array(value, [true, false])
+    end
 
     ##
     # File path of the config YAML. Creates directory if not yet existing.
@@ -97,6 +176,12 @@ module Tildeverse
       #             at tilde.town. This is run every day at midnight, so the
       #             results will likely be more accurate than manual scraping.
       @get_type@
+
+      # Should new data be automatically downloaded every 24 hrs?
+      @get_daily@
+
+      # Should a website be generated along with the JSON output?
+      @generate_html@
 
       # The date that the data was last updated.
       # Should be in the form: 'YYYY-MM-DD'
