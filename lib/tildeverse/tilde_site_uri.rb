d46e38474202ae87a729c039e002fc127424e7b3
Paul Thompson <nossidge@gmail.com>
2018-06-27 10:44:04 +0100

    add 'tilde_site_uri.rb' and spec

---
+++ lib/tildeverse/tilde_site_uri.rb
@@ -0,0 +1,177 @@
+#!/usr/bin/env ruby
+
+module Tildeverse
+  ##
+  # Wrapper class for Ruby's URI module.
+  # Adds some functionality specific to Tilde user list URIs.
+  #
+  # Delegates missing methods to the underlying
+  # {URI}[https://ruby-doc.org/stdlib-2.3.3/libdoc/uri/rdoc/URI.html]
+  # instance {#uri}
+  #
+  class TildeSiteURI
+    ##
+    # @return [URI] the URI of the user list.
+    #   All other attributes are based on this by default.
+    #
+    attr_reader :uri
+
+    ##
+    # @!attribute name
+    # @return [String] the name of the website
+    # @example
+    #   'example.com'
+    #   'tilde.town'
+
+    ##
+    # @!attribute url_root
+    # @return [String] the root URL of the website
+    # @example
+    #   'http://example.com/'
+    #   'https://tilde.town/'
+
+    ##
+    # @!attribute url_list
+    # @return [String] the URL of the user list
+    # @example
+    #   'http://example.com/users.html'
+    #   'https://tilde.town/~dan/users.json'
+
+    ##
+    # @!attribute homepage_format
+    # @return [String]
+    #   the format that the site uses to map users to their homepage.
+    # @example
+    #   'https://tilde.town/~USER/'
+    #   'https://USER.remotes.club/'
+
+    ##
+    # Make setters and getters that default to method +default_<attr>+
+    # if not explicitly set. This way users of the class can specify their
+    # own values if the defaults are not appropriate.
+    #
+    # Because the code is using ||, a variable can be restored to the
+    # default by setting its value to nil.
+    #
+    def self.use_default_if_missing(attr)
+      class_eval(<<-CODE, __FILE__, __LINE__ + 1)
+        def #{attr}=(val)
+          @#{attr} = val
+        end
+        def #{attr}
+          @#{attr} || default_#{attr}
+        end
+      CODE
+    end
+    use_default_if_missing(:name)
+    use_default_if_missing(:url_root)
+    use_default_if_missing(:url_list)
+    use_default_if_missing(:homepage_format)
+
+    ##
+    # @param [String, URI] uri
+    #   {URI}[https://ruby-doc.org/stdlib-2.3.3/libdoc/uri/rdoc/URI.html]
+    #   object to which this class acts as a wrapper
+    #
+    def initialize(uri)
+      @uri = uri
+      @uri = URI(URI.escape(uri)) unless uri.is_a?(URI::HTTP)
+      validate_uri
+    rescue
+      raise(NotHTTPError)
+    end
+
+    ############################################################################
+
+    ##
+    # Use {#homepage_format} to map the user to their homepage URL.
+    #
+    # @param [String] user The name of the user.
+    # @return [String] user's homepage.
+    # @example
+    #   site = TildeSiteURI.new('http://tilde.town/~dan/users.json')
+    #   site.user_page('imt')
+    #   # => 'https://tilde.town/~imt/'
+    # @example
+    #   site = TildeSiteURI.new('https://www.remotes.club/')
+    #   site.homepage_format = 'https://USER.remotes.club/'
+    #   site.user_page('imt')
+    #   # => 'https://imt.remotes.club/'
+    #
+    def user_page(user)
+      output = homepage_format.sub('USER', user)
+
+      # Throw error if 'homepage_format' does not contain USER substring.
+      if homepage_format == output
+        msg  = '#homepage_format should replace USER in the format eg: '
+        msg += 'http://www.example.com/~USER/'
+        raise ArgumentError, msg
+      end
+      URI(URI.escape(output))
+    end
+
+    ##
+    # Use {#name} to map the user to their email address
+    #
+    # @param [String] user The name of the user
+    # @return [String] user's email address
+    # @example
+    #   site = TildeSiteURI.new('http://tilde.town/~dan/users.json')
+    #   site.user_email('nossidge')
+    #   # => 'nossidge@tilde.town'
+    # @note
+    #   On most Tilde servers, this is valid for local email only
+    #
+    def user_email(user)
+      user.to_s + '@' + name
+    end
+
+    ############################################################################
+
+    private
+
+    def default_name
+      uri.host
+    end
+
+    def default_url_root
+      uri.scheme + '://' + uri.host
+    end
+
+    def default_url_list
+      uri.to_s
+    end
+
+    def default_homepage_format
+      url_root + '/~USER/'
+    end
+
+    # Raise an error if the URI is not hte correct type
+    def validate_uri
+      return true if uri.is_a?(URI::HTTP)
+      raise NotHTTPError
+    end
+
+    # Exception to raise on invalid URI input
+    class NotHTTPError < URI::InvalidURIError
+      def initialize(msg = 'Tilde URI must be HTTP or HTTPS')
+        super msg
+      end
+    end
+
+    # Delegate unknown methods to the {#uri} instance
+    def method_missing(method, *args)
+      if uri.respond_to?(method)
+        uri.send(method, *args)
+      else
+        raise NoMethodError, method
+      end
+    end
+
+    # Respond to unknown methods as the {#uri} instance
+    def respond_to_missing?(method, include_private = false)
+      return true if uri.respond_to?(method)
+      super
+    end
+  end
+end
---
+++ spec/tilde_site_uri_spec.rb
@@ -0,0 +1,179 @@
+#!/usr/bin/env ruby
+
+describe 'Tildeverse::TildeSiteURI' do
+  let(:example_data) do
+    {
+      'https://tilde.town/~dan/users.json' => {
+        name:             'tilde.town',
+        url_root:         'https://tilde.town',
+        url_list:         'https://tilde.town/~dan/users.json',
+        homepage_format:  'https://tilde.town/~USER/'
+      },
+      'http://example.com' => {
+        name:             'example.com',
+        url_root:         'http://example.com',
+        url_list:         'http://example.com',
+        homepage_format:  'http://example.com/~USER/'
+      }
+    }
+  end
+
+  describe '#new' do
+    it 'should fail if URI is not HTTP or HTTPS' do
+      [
+        'httpss://tilde.town/~dan/users.json',
+        'ftp://tilde.town/~dan/users.json',
+        URI('ftp://tilde.town/~dan/users.json'),
+        URI('mailto:foo@example.com'),
+        'foo',
+        123
+      ].each do |uri|
+        expect do
+          Tildeverse::TildeSiteURI.new(uri)
+        end.to raise_error(
+          URI::InvalidURIError,
+          'Tilde URI must be HTTP or HTTPS'
+        )
+      end
+    end
+
+    it 'should correctly create an instance' do
+      [
+        'http://tilde.town/~dan/users.json',
+        'https://tilde.town/~dan/users.json',
+        URI('http://tilde.town/~dan/users.json'),
+        URI('https://tilde.town/~dan/users.json')
+      ].each do |uri|
+        expect do
+          Tildeverse::TildeSiteURI.new(uri)
+        end.not_to raise_error
+      end
+    end
+
+    it 'should return the expected default values' do
+      example_data.each do |uri, expectations|
+        obj = Tildeverse::TildeSiteURI.new(uri)
+        expectations.each do |message, result|
+          expect(obj.send(message)).to eq result
+        end
+      end
+    end
+  end
+
+  describe '#uri' do
+    it 'should be a URI::HTTP' do
+      [
+        'http://tilde.town/~dan/users.json',
+        'https://tilde.town/~dan/users.json'
+      ].each do |uri|
+        obj = Tildeverse::TildeSiteURI.new(uri)
+        expect(obj.uri).to be_a URI::HTTP
+      end
+    end
+
+    it 'should be the delegate object on #method_missing' do
+      obj = Tildeverse::TildeSiteURI.new('http://example.com')
+      expect { obj.host            }.not_to raise_error
+      expect { obj.scheme          }.not_to raise_error
+      expect { obj.foo             }.to raise_error NoMethodError
+      expect { obj.bar             }.to raise_error NoMethodError
+
+      # Messing with #method_missing should not break defined methods
+      expect { obj.name            }.not_to raise_error
+      expect { obj.homepage_format }.not_to raise_error
+      expect { obj.uri.host        }.not_to raise_error
+      expect { obj.uri.scheme      }.not_to raise_error
+    end
+
+    it 'should be the delegate object on #respond_to_missing?' do
+      obj = Tildeverse::TildeSiteURI.new('http://example.com')
+      expect(obj.respond_to?(:host)           ).to be true
+      expect(obj.respond_to?(:scheme)         ).to be true
+      expect(obj.respond_to?(:foo)            ).to be false
+      expect(obj.respond_to?(:bar)            ).to be false
+
+      # Messing with #respond_to_missing? should not break defined methods
+      expect(obj.respond_to?(:name)           ).to be true
+      expect(obj.respond_to?(:homepage_format)).to be true
+      expect(obj.uri.respond_to?(:host)       ).to be true
+      expect(obj.uri.respond_to?(:scheme)     ).to be true
+    end
+  end
+
+  describe '#name' do
+    it 'should be able to overwrite and restore default values' do
+      uri, expectations = example_data.first
+      obj = Tildeverse::TildeSiteURI.new(uri)
+      expect(obj.name).to eq expectations[:name]
+      obj.name = 'foobar'
+      expect(obj.name).to eq 'foobar'
+      obj.name = nil
+      expect(obj.name).to eq expectations[:name]
+    end
+  end
+
+  describe '#url_root' do
+    it 'should be able to overwrite and restore default values' do
+      uri, expectations = example_data.first
+      obj = Tildeverse::TildeSiteURI.new(uri)
+      expect(obj.url_root).to eq expectations[:url_root]
+      obj.url_root = 'foobar'
+      expect(obj.url_root).to eq 'foobar'
+      obj.url_root = nil
+      expect(obj.url_root).to eq expectations[:url_root]
+    end
+  end
+
+  describe '#url_list' do
+    it 'should be able to overwrite and restore default values' do
+      uri, expectations = example_data.first
+      obj = Tildeverse::TildeSiteURI.new(uri)
+      expect(obj.url_list).to eq expectations[:url_list]
+      obj.url_list = 'foobar'
+      expect(obj.url_list).to eq 'foobar'
+      obj.url_list = nil
+      expect(obj.url_list).to eq expectations[:url_list]
+    end
+  end
+
+  describe '#homepage_format' do
+    it 'should be able to overwrite and restore default values' do
+      uri, expectations = example_data.first
+      obj = Tildeverse::TildeSiteURI.new(uri)
+      expect(obj.homepage_format).to eq expectations[:homepage_format]
+      obj.homepage_format = 'foobar'
+      expect(obj.homepage_format).to eq 'foobar'
+      obj.homepage_format = nil
+      expect(obj.homepage_format).to eq expectations[:homepage_format]
+    end
+  end
+
+  describe '#user_page(user)' do
+    it 'should return the expected URI' do
+      uri, = example_data.first
+      obj = Tildeverse::TildeSiteURI.new(uri)
+      result = obj.user_page('foobar')
+      expect(result).to be_a URI::HTTP
+      expect(result.to_s).to eq 'https://tilde.town/~foobar/'
+    end
+
+    it 'should error if template is in an incorrect format' do
+      uri, = example_data.first
+      obj = Tildeverse::TildeSiteURI.new(uri)
+      obj.homepage_format = 'https://tilde.town/~FOOBAR/'
+      expect do
+        obj.user_page('foobar')
+      end.to raise_error ArgumentError
+    end
+  end
+
+  describe '#user_email(user)' do
+    it 'should return the expected email address' do
+      uri, = example_data.first
+      obj = Tildeverse::TildeSiteURI.new(uri)
+      result = obj.user_email('foobar')
+      expect(result).to be_a String
+      expect(result).to eq 'foobar@tilde.town'
+    end
+  end
+end
