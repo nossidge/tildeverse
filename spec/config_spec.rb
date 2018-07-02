025c7ec05db160f7c35276c579d29b9c2eab8087
Paul Thompson <nossidge@gmail.com>
2018-07-02 14:21:37 +0100

    spec refactor

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -2,10 +2,6 @@
 
 describe 'Tildeverse::Config' do
 
-  def instance
-    Tildeverse::Config.new(temp_file)
-  end
-
   def temp_file
     Tildeverse::Files.dir_root + 'config/temp_config.yml'
   end
@@ -14,7 +10,11 @@ describe 'Tildeverse::Config' do
     File.delete(temp_file) if File.exist?(temp_file)
   end
 
-  def default_values_hash
+  let(:instance) do
+    Tildeverse::Config.new(temp_file)
+  end
+
+  let(:default_values_hash) do
     {
       update_type:      'fetch',
       update_frequency: 'day',
@@ -23,37 +23,36 @@ describe 'Tildeverse::Config' do
     }
   end
 
-  after(:all) do
+  after(:each) do
     kill_temp_file
   end
 
   ##############################################################################
 
-  # There should be no trouble with default file location
-  it '#new(default)' do
-    config = Tildeverse::Config.new
-    expect(config.class).to eq Tildeverse::Config
-    expect(config.filepath).to eq Tildeverse::Files.config_yml
-  end
+  describe '#new' do
+    it 'should correctly instantiate using default file location' do
+      config = Tildeverse::Config.new
+      expect(config.class).to eq Tildeverse::Config
+      expect(config.filepath).to eq Tildeverse::Files.config_yml
+    end
 
-  # There should be no trouble with default file location
-  it '#new(temp_file)' do
-    kill_temp_file
-    config = Tildeverse::Config.new(temp_file)
-    expect(config.filepath).to eq temp_file
+    it 'should correctly instantiate using file passed as a param' do
+      config = Tildeverse::Config.new(temp_file)
+      expect(config.filepath).to eq temp_file
 
-    # Make sure the options match the defaults
-    default_values_hash.each do |k, v|
-      expect(config.send(k)).to eq v
+      # Make sure the options match the defaults
+      default_values_hash.each do |k, v|
+        expect(config.send(k)).to eq v
+      end
     end
   end
 
-  # The default values should be sensible
-  it '#default_values' do
-    kill_temp_file
-    config = Tildeverse::Config.new(temp_file)
-    dv = config.send(:default_values)
-    expect(dv).to eq default_values_hash
+  describe '#default_values' do
+    it 'should use sensible default values' do
+      config = Tildeverse::Config.new(temp_file)
+      dv = config.send(:default_values)
+      expect(dv).to eq default_values_hash
+    end
   end
 
   ##############################################################################
@@ -69,139 +68,143 @@ describe 'Tildeverse::Config' do
     end
   end
 
-  it '#update_type=' do
-    valid = %w[scrape fetch]
-    invalid = [123, nil, Integer, true, 'always', 'day', 'week', 'month']
-    test_raise_error(valid, invalid) do |i|
-      instance.send(:validate_update_type, i)
-    end
-    test_raise_error(valid, invalid) do |i|
-      instance.update_type = i
-    end
-  end
-
-  it '#update_frequency=' do
-    valid = %w[always day week month]
-    invalid = [123, nil, Integer, true, 'scrape', 'fetch']
-    test_raise_error(valid, invalid) do |i|
-      instance.send(:validate_update_frequency, i)
-    end
-    test_raise_error(valid, invalid) do |i|
-      instance.update_frequency, = i
+  describe '#update_type=' do
+    it 'should correctly validate input' do
+      valid = %w[scrape fetch]
+      invalid = [123, nil, Integer, true, 'always', 'day', 'week', 'month']
+      test_raise_error(valid, invalid) do |i|
+        instance.send(:validate_update_type, i)
+      end
+      test_raise_error(valid, invalid) do |i|
+        instance.update_type = i
+      end
     end
   end
 
-  it '#generate_html=' do
-    valid = [true, false]
-    invalid = [123, nil, Integer, 'scrape', 'always']
-    test_raise_error(valid, invalid) do |i|
-      instance.send(:validate_generate_html, i)
+  describe '#update_frequency=' do
+    it 'should correctly validate input' do
+      valid = %w[always day week month]
+      invalid = [123, nil, Integer, true, 'scrape', 'fetch']
+      test_raise_error(valid, invalid) do |i|
+        instance.send(:validate_update_frequency, i)
+      end
+      test_raise_error(valid, invalid) do |i|
+        instance.update_frequency, = i
+      end
     end
-    test_raise_error(valid, invalid) do |i|
-      instance.generate_html = i
+  end
+
+  describe '#generate_html=' do
+    it 'should correctly validate input' do
+      valid = [true, false]
+      invalid = [123, nil, Integer, 'scrape', 'always']
+      test_raise_error(valid, invalid) do |i|
+        instance.send(:validate_generate_html, i)
+      end
+      test_raise_error(valid, invalid) do |i|
+        instance.generate_html = i
+      end
+    end
+  end
+
+  ##############################################################################
+
+  describe '#update' do
+    it "should use today's date" do
+      config = Tildeverse::Config.new(temp_file)
+      expect(config.updated_on).to eq default_values_hash[:updated_on]
+      config.update
+      expect(config.updated_on).to eq Date.today
+
+      # Create 2nd instance with same save file
+      # Should be same date
+      config = Tildeverse::Config.new(temp_file)
+      expect(config.updated_on).to eq Date.today
     end
   end
 
   ##############################################################################
 
-  # Make sure #update uses today's date
-  it '#update' do
-    kill_temp_file
-    config = Tildeverse::Config.new(temp_file)
-    expect(config.updated_on).to eq default_values_hash[:updated_on]
-    config.update
-    expect(config.updated_on).to eq Date.today
-
-    # Create 2nd instance with same save file
-    # Should be same date
-    config = Tildeverse::Config.new(temp_file)
-    expect(config.updated_on).to eq Date.today
-  end
-
-  ##############################################################################
-
-  it "#update_required? <- update_frequency = 'always'" do
-    kill_temp_file
-    config = Tildeverse::Config.new(temp_file)
-    config.update_frequency = 'always'
-    allow(config).to receive(:updated_on).and_return(Date.today + 1)
-    expect(config.update_required?).to eq true
-    allow(config).to receive(:updated_on).and_return(Date.today)
-    expect(config.update_required?).to eq true
-    allow(config).to receive(:updated_on).and_return(Date.today - 1)
-    expect(config.update_required?).to eq true
-  end
-
-  it "#update_required? <- update_frequency = 'day'" do
-    kill_temp_file
-    config = Tildeverse::Config.new(temp_file)
-    config.update_frequency = 'day'
-    allow(config).to receive(:updated_on).and_return(Date.today + 1)
-    expect(config.update_required?).to eq false
-    allow(config).to receive(:updated_on).and_return(Date.today)
-    expect(config.update_required?).to eq false
-    allow(config).to receive(:updated_on).and_return(Date.today - 1)
-    expect(config.update_required?).to eq true
-  end
-
-  it "#update_required? <- update_frequency = 'week'" do
-    kill_temp_file
-    config = Tildeverse::Config.new(temp_file)
-    config.update_frequency = 'week'
-
-    # Test with today's date, changing '#updated_on' date
-    past   = Date.today - 50
-    future = Date.today + 50
-    (past..future).each do |date|
-      allow(config).to receive(:updated_on).and_return(date)
-      mon_date = Date.today - Date.today.cwday + 1
-      sun_date = mon_date + 6
-      week_range = (mon_date..sun_date)
-      should_be = !week_range.include?(date)
-      expect(config.update_required?).to eq should_be
+  describe "#update_required?" do
+    it "should work correctly when update_frequency = 'always'" do
+      config = Tildeverse::Config.new(temp_file)
+      config.update_frequency = 'always'
+      allow(config).to receive(:updated_on).and_return(Date.today + 1)
+      expect(config.update_required?).to eq true
+      allow(config).to receive(:updated_on).and_return(Date.today)
+      expect(config.update_required?).to eq true
+      allow(config).to receive(:updated_on).and_return(Date.today - 1)
+      expect(config.update_required?).to eq true
     end
 
-    # Test boundaries using explicit values, and a variable '#date_today'
-    date_mon = Date.new(2018, 6, 18)
-    date_sun = Date.new(2018, 6, 24)
-    (date_mon..date_sun).each do |date|
-      allow(config).to receive(:date_today).and_return(date)
+    it "should work correctly when update_frequency = 'day'" do
+      config = Tildeverse::Config.new(temp_file)
+      config.update_frequency = 'day'
+      allow(config).to receive(:updated_on).and_return(Date.today + 1)
+      expect(config.update_required?).to eq false
+      allow(config).to receive(:updated_on).and_return(Date.today)
+      expect(config.update_required?).to eq false
+      allow(config).to receive(:updated_on).and_return(Date.today - 1)
+      expect(config.update_required?).to eq true
+    end
+
+    it "should work correctly when update_frequency = 'week'" do
+      config = Tildeverse::Config.new(temp_file)
+      config.update_frequency = 'week'
+
+      # Test with today's date, changing '#updated_on' date
+      past   = Date.today - 50
+      future = Date.today + 50
+      (past..future).each do |date|
+        allow(config).to receive(:updated_on).and_return(date)
+        mon_date = Date.today - Date.today.cwday + 1
+        sun_date = mon_date + 6
+        week_range = (mon_date..sun_date)
+        should_be = !week_range.include?(date)
+        expect(config.update_required?).to eq should_be
+      end
+
+      # Test boundaries using explicit values, and a variable '#date_today'
+      date_mon = Date.new(2018, 6, 18)
+      date_sun = Date.new(2018, 6, 24)
+      (date_mon..date_sun).each do |date|
+        allow(config).to receive(:date_today).and_return(date)
+        allow(config).to receive(:updated_on).and_return(Date.new(2018, 6, 16))
+        expect(config.update_required?).to eq true
+        allow(config).to receive(:updated_on).and_return(Date.new(2018, 6, 17))
+        expect(config.update_required?).to eq true
+        allow(config).to receive(:updated_on).and_return(Date.new(2018, 6, 18))
+        expect(config.update_required?).to eq false
+        allow(config).to receive(:updated_on).and_return(Date.new(2018, 6, 19))
+        expect(config.update_required?).to eq false
+      end
+    end
+
+    it "should work correctly when update_frequency = 'month'" do
+      config = Tildeverse::Config.new(temp_file)
+      config.update_frequency = 'month'
+
+      allow(config).to receive(:date_today).and_return(Date.new(2018, 6, 16))
       allow(config).to receive(:updated_on).and_return(Date.new(2018, 6, 16))
-      expect(config.update_required?).to eq true
-      allow(config).to receive(:updated_on).and_return(Date.new(2018, 6, 17))
-      expect(config.update_required?).to eq true
-      allow(config).to receive(:updated_on).and_return(Date.new(2018, 6, 18))
       expect(config.update_required?).to eq false
-      allow(config).to receive(:updated_on).and_return(Date.new(2018, 6, 19))
+      allow(config).to receive(:updated_on).and_return(Date.new(2018, 6, 1))
+      expect(config.update_required?).to eq false
+      allow(config).to receive(:updated_on).and_return(Date.new(2018, 5, 31))
+      expect(config.update_required?).to eq true
+      allow(config).to receive(:updated_on).and_return(Date.new(2018, 5, 16))
+      expect(config.update_required?).to eq true
+      allow(config).to receive(:updated_on).and_return(Date.new(2017, 6, 16))
+      expect(config.update_required?).to eq true
+      allow(config).to receive(:updated_on).and_return(Date.new(2017, 10, 1))
+      expect(config.update_required?).to eq true
+
+      # If '#updated_on' is in the future, wait for that date
+      allow(config).to receive(:updated_on).and_return(Date.new(2020, 6, 16))
+      expect(config.update_required?).to eq false
+      allow(config).to receive(:updated_on).and_return(Date.new(2018, 6, 17))
+      expect(config.update_required?).to eq false
+      allow(config).to receive(:updated_on).and_return(Date.new(2018, 7, 1))
       expect(config.update_required?).to eq false
     end
-  end
-
-  it "#update_required? <- update_frequency = 'month'" do
-    kill_temp_file
-    config = Tildeverse::Config.new(temp_file)
-    config.update_frequency = 'month'
-
-    allow(config).to receive(:date_today).and_return(Date.new(2018, 6, 16))
-    allow(config).to receive(:updated_on).and_return(Date.new(2018, 6, 16))
-    expect(config.update_required?).to eq false
-    allow(config).to receive(:updated_on).and_return(Date.new(2018, 6, 1))
-    expect(config.update_required?).to eq false
-    allow(config).to receive(:updated_on).and_return(Date.new(2018, 5, 31))
-    expect(config.update_required?).to eq true
-    allow(config).to receive(:updated_on).and_return(Date.new(2018, 5, 16))
-    expect(config.update_required?).to eq true
-    allow(config).to receive(:updated_on).and_return(Date.new(2017, 6, 16))
-    expect(config.update_required?).to eq true
-    allow(config).to receive(:updated_on).and_return(Date.new(2017, 10, 1))
-    expect(config.update_required?).to eq true
-
-    # If '#updated_on' is in the future, wait for that date
-    allow(config).to receive(:updated_on).and_return(Date.new(2020, 6, 16))
-    expect(config.update_required?).to eq false
-    allow(config).to receive(:updated_on).and_return(Date.new(2018, 6, 17))
-    expect(config.update_required?).to eq false
-    allow(config).to receive(:updated_on).and_return(Date.new(2018, 7, 1))
-    expect(config.update_required?).to eq false
   end
 end
--- spec/pfhawkins_spec.rb
+++ spec/pfhawkins_spec.rb
@@ -1,51 +1,66 @@
 #!/usr/bin/env ruby
 
 describe 'Tildeverse::PFHawkins' do
-  def instance
+  let(:instance) do
     Tildeverse::PFHawkins.new
   end
 
-  it '#url_html' do
-    uri = URI(instance.url_html)
-    res = Net::HTTP.get_response(uri)
-    expect(res.code).to eq '200'
-  end
-
-  it '#url_json' do
-    uri = URI(instance.url_json)
-    res = Net::HTTP.get_response(uri)
-    expect(res.code).to eq '200'
-  end
-
-  it '#servers' do
-    servers = instance.servers
-    expect(servers).to be_a Array
-    sites = instance.sites
-    expect(sites).to be_a Array
-    boxes = instance.boxes
-    expect(boxes).to be_a Array
-    expect(servers).to eq sites
-    expect(servers).to eq boxes
-  end
-
-  it '#new?' do
-    alter_servers = ->(&block) do
-      pfhawkins = Tildeverse::PFHawkins.new
-      expect(pfhawkins.new?).to be false
-      servers = pfhawkins.servers.dup
-      block.call(servers)
-      allow(pfhawkins).to receive(:server_list_cache).and_return(servers)
-      expect(pfhawkins.new?).to be true
+  describe '#url_html' do
+    it 'should respond correctly to HTTP GET' do
+      uri = URI(instance.url_html)
+      res = Net::HTTP.get_response(uri)
+      expect(res.code).to eq '200'
     end
-    alter_servers.call { |servers| servers << 'new-foo.com' }
-    alter_servers.call { |servers| servers.pop }
   end
 
-  it '#puts_if_new' do
-    pfhawkins = Tildeverse::PFHawkins.new
-    allow(pfhawkins).to receive(:new?).and_return(true)
-    msg = "-- New Tilde Boxes!\n" + pfhawkins.url_html
-    expect(STDOUT).to receive(:puts).with(msg)
-    pfhawkins.puts_if_new
+  describe '#url_json' do
+    it 'should respond correctly to HTTP GET' do
+      uri = URI(instance.url_json)
+      res = Net::HTTP.get_response(uri)
+      expect(res.code).to eq '200'
+    end
+  end
+
+  describe '#servers' do
+    it 'should return an array' do
+      servers = instance.servers
+      expect(servers).to be_a Array
+    end
+    it 'should alias to #sites' do
+      sites = instance.sites
+      expect(sites).to eq instance.servers
+    end
+    it 'should alias to #boxes' do
+      boxes = instance.boxes
+      expect(boxes).to eq instance.servers
+    end
+  end
+
+  describe '#new?' do
+    it 'should return the correct boolean' do
+      alter_servers = ->(&block) do
+        pfhawkins = Tildeverse::PFHawkins.new
+        expect(pfhawkins.new?).to be false
+        servers = pfhawkins.servers.dup
+        block.call(servers)
+        allow(pfhawkins).to receive(:server_list_cache).and_return(servers)
+        expect(pfhawkins.new?).to be true
+      end
+      alter_servers.call { |servers| servers << 'new-foo.com' }
+      alter_servers.call { |servers| servers.pop }
+    end
+  end
+
+  describe '#puts_if_new' do
+    it 'should not output if #new? is false' do
+      expect(STDOUT).to_not receive(:puts)
+      instance.puts_if_new
+    end
+    it 'should output to STDOUT if #new? is true' do
+      allow(instance).to receive(:new?).and_return(true)
+      msg = "-- New Tilde Boxes!\n" + instance.url_html
+      expect(STDOUT).to receive(:puts).with(msg)
+      instance.puts_if_new
+    end
   end
 end
